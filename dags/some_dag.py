b0c29644c2804bbf93b6fe2a3d330051728c7baa
FistGang <20520671@gm.uit.edu.vn>
2024-06-10 14:13:02 +0700

    feat: rewrite DAG object using TaskFlow API

--- dags/some_dag.py
+++ dags/some_dag.py
@@ -1,27 +1,63 @@
+import json
+
 import pendulum
-from airflow.decorators import dag, task, task_group
 
+from airflow.decorators import dag, task
+@dag(
+    schedule=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["example"],
+)
+def tutorial_taskflow_api():
+    """
+    ### TaskFlow API Tutorial Documentation
+    This is a simple data pipeline example which demonstrates the use of
+    the TaskFlow API using three simple tasks for Extract, Transform, and Load.
+    Documentation that goes along with the Airflow TaskFlow API tutorial is
+    located
+    [here](https://airflow.apache.org/docs/apache-airflow/stable/tutorial_taskflow_api.html)
+    """
+    @task()
+    def extract():
+        """
+        #### Extract task
+        A simple Extract task to get data ready for the rest of the data
+        pipeline. In this case, getting data is simulated by reading from a
+        hardcoded JSON string.
+        """
+        data_string = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
 
-@dag(start_date=pendulum.now())
-def some_dag():
-    @task
-    def some_task_1():
-        print("hello world 1")
+        order_data_dict = json.loads(data_string)
+        return order_data_dict
 
-    @task
-    def some_task_2():
-        print("hello world 2")
+    @task(multiple_outputs=True)
+    def transform(order_data_dict: dict):
+        """
+        #### Transform task
+        A simple Transform task which takes in the collection of order data and
+        computes the total order value.
+        """
+        total_order_value = 0
 
-    @task
-    def some_task_3():
-        print("hello world 3")
+        for value in order_data_dict.values():
+            total_order_value += value
 
-    @task_group
-    def example_group():
-        some_task_2()
-        some_task_3()
+        return {"total_order_value": total_order_value}
 
-    some_task_1() >> example_group()
+    @task()
+    def load(total_order_value: float):
+        """
+        #### Load task
+        A simple Load task which takes in the result of the Transform task and
+        instead of saving it to end user review, just prints it out.
+        """
 
+        print(f"Total order value is: {total_order_value:.2f}")
 
-some_dag()
+    order_data = extract()
+    order_summary = transform(order_data)
+    load(order_summary["total_order_value"])
+
+tutorial_taskflow_api()
+
